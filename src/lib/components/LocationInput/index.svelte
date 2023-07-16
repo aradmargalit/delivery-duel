03f0491e51d60bb0ec90c3b070b237627932b011
Arad Margalit <aradmar@gmail.com>
2023-07-16 10:17:27 -0700

    fix int issue

--- src/lib/components/LocationInput/index.svelte
+++ src/lib/components/LocationInput/index.svelte
@@ -3,39 +3,44 @@
   import { locationState } from '$lib/stores/location';
 
   let locationError = '';
-  let coordinateState = 'determining your location...';
+  let coordinateState = '';
 
-  // Determine if we can get the user location from the browser
-  // Only safe on client
-  if (browser) {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          coordinateState = '';
-          locationState.setCoordinates({
-            lat: position.coords.latitude,
-            lon: position.coords.longitude
-          });
-        },
-        (positionError) => {
-          locationError = positionError.message;
-        }
-      );
+  function tryCurrentLocation() {
+    coordinateState = 'determining your location...';
+    // Determine if we can get the user location from the browser
+    // Only safe on client
+    if (browser) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            coordinateState = '';
+            locationState.setCoordinates({
+              lat: position.coords.latitude,
+              lon: position.coords.longitude
+            });
+          },
+          (positionError) => {
+            locationError = positionError.message;
+          }
+        );
+      }
     }
   }
 </script>
 
 <div class="flex flex-col">
-  <p class="mt-5">{coordinateState}</p>
   <div class="field-row">
-    <label for="locationInput">Enter your location</label>
+    <label for="locationInput">Enter desired location</label>
     <input
       bind:value={$locationState.manualLocation}
       id="locationInput"
       type="text"
-      placeholder="Irvine, CA"
+      placeholder="e.g. Irvine, CA"
     />
+    <p>or</p>
+    <button on:click={tryCurrentLocation}>Use current location</button>
   </div>
+  <p class="mt-5">{coordinateState}</p>
   {#if locationError}
     <p class="mt-5 text-red-500">Could not find your location: {locationError}</p>
   {:else if $locationState.coordinates}
