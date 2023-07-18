644ab3c6fc1c8db6e6341047c29541b1346c9ae8
Arad Margalit <aradmargalit@users.noreply.github.com>
2023-07-18 12:36:10 -0700

    cosmetics, favicon (#9)

--- src/lib/components/RestaurantCard/index.svelte
+++ src/lib/components/RestaurantCard/index.svelte
@@ -4,11 +4,14 @@
   export let restaurant: Restaurant;
 </script>
 
-<div class="window mb-5 flex h-48 w-96 flex-col items-center justify-center">
+<div class="window mb-5 flex h-48 flex-col items-center justify-center md:w-[30rem]">
   <div class="title-bar w-full">
     <p class="title-bar-text">{restaurant.title}</p>
     <div class="title-bar-controls">
-      <button aria-label="Site" on:click|stopPropagation={() => window.open(restaurant.url, '_blank')}>
+      <button
+        aria-label="Site"
+        on:click|stopPropagation={() => window.open(restaurant.url, '_blank')}
+      >
         Website
       </button>
       <button disabled aria-label="Close" />
@@ -17,6 +20,6 @@
   <img src={restaurant.imageUrl} alt="restaurant" class="w-full overflow-hidden object-cover" />
   <div class="flex flex-col items-center justify-center">
     <p><em>{restaurant.tags.join(', ')}</em></p>
-    <p><em>{restaurant.location}</em></p>
+    <p class="mt-1">{restaurant.location}</p>
   </div>
 </div>
